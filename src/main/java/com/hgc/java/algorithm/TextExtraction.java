337caf30d42c2618b046516fd955dc8f88e62ec0
guicheng.huang <guicheng.huang@bestwehotel.com>
2017-06-06 09:42:11 +0800

    修改.

---
+++ src/main/java/com/hgc/java/algorithm/TextExtraction.java
@@ -0,0 +1,25 @@
+package com.hgc.java.algorithm;
+
+import java.io.IOException;
+
+
+import java.util.List;
+
+import org.ansj.domain.Term;
+import org.ansj.splitWord.analysis.ToAnalysis;
+
+import jdk.internal.org.objectweb.asm.tree.analysis.Analyzer;
+
+/**
+ * @Description:文本提取
+ * @author guicheng.huang
+ * @date: 2017年5月31日 下午5:43:12
+ * @version V0.0.1
+ */
+public class TextExtraction {
+
+	 public static void main(String[] args) throws IOException {  
+		 List<Term> parse = ToAnalysis.parse("WawaCluster[] clusters = kmeans.Clusters;");    
+		 System.out.println(parse);
+	 }  
+}
